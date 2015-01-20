a2ceba9b9b21c5e067e8364d0524896673f58c51
Topstitch <kallen@spoonflower.com>
2015-01-20 16:10:05 -0500

    Pass third test...equivalent objects

--- currency.rb
+++ currency.rb
@@ -1,6 +1,13 @@
 class Currency
+  attr_reader :amount, :currency_code
   def initialize(amount, currency_code)
     @amount = amount
     @currency_code = currency_code
   end
+
+  def ==(other_money)
+    self.amount == other_money.amount
+    self.currency_code == other_money.currency_code
+  end
+
 end
--- currency_test.rb
+++ currency_test.rb
@@ -11,4 +11,15 @@ class CurrencyTest <Minitest::Test
     assert Currency.new("", "")
   end
 
+  def test_two_currency_objects_are_equivalent
+    my_dollar = Currency.new(1, "USD")
+    your_dollar = Currency.new(1, "USD")
+    assert my_dollar == your_dollar
+  end
+
 end
+
+
+
+
+#http://www.skorks.com/2009/09/ruby-equality-and-object-comparison/
