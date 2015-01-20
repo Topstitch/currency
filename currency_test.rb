8b64ebe8318f6653114e9aff6df010eed572e121
Topstitch <kallen@spoonflower.com>
2015-01-20 15:36:08 -0500

    Pass second test

--- currency_test.rb
+++ currency_test.rb
@@ -6,4 +6,9 @@ class CurrencyTest <Minitest::Test
   def test_currency_class_exists
     assert Currency
   end
+
+  def test_currency_class_takes_two_parameters
+    assert Currency.new("", "")
+  end
+
 end
